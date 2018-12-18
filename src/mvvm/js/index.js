78ba1acd1fcf819df083620585d3f2e8819dc39b
xiaran <xiaran@polomi.com>
2018-12-18 16:57:32 +0800

    mvvm sample editing

---
+++ src/mvvm/js/index.js
@@ -0,0 +1,29 @@
+function Vue(options) {
+    var self = this;
+    this.data = options.data;
+    this.methods = options.methods;
+
+    Object.keys(this.data).forEach(function (key) {
+        self.proxyKey(key); // 为每个属性添加代理
+    })
+
+    observe(this.data);
+    new Compile(options.el, this);
+    options.mounted.all(this)
+}
+
+Vue.prototype = {
+    proxyKeys: function (key) {
+        var self = this;
+        Object.defineProperty(this, key, {
+            emumerable: false,
+            configurable: true,
+            get: function () {
+                return self.data[key];
+            },
+            set: function (newVal) {
+                self.data[key] = newVal;
+            }
+        })
+    }
+}
