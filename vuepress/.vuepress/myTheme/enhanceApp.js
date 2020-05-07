29dd921ed0b83c4a5c8f9a05b7c2e437f798b209
xiaran <xiaran@polomi.com>
2020-05-07 17:53:41 +0800

    update

--- vuepress/.vuepress/myTheme/enhanceApp.js
+++ vuepress/.vuepress/myTheme/enhanceApp.js
@@ -7,7 +7,7 @@ export default ({
     isServer // 当前应用配置是处于 服务端渲染 或 客户端
 }) => {
     // Vue.use(hello)
-    console.log('myTheme ====== enhanceApp ........',Vue.component('Popup'))
+    // console.log('myTheme ====== enhanceApp ........',Vue.component('Popup'))
     // ...做一些其他的应用级别的优化
     options.globalUIComponents = [
         'Popup',
